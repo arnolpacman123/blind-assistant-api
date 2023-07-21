8baf5acc7cc4982de4ba552570ee2c341b535676
Arnol Guevara <arnolpacman@gmail.com>
2023-07-20 20:15:22 -0400

    updated

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
-    forbidNonWhitelisted: true,
     transform: true,
   }));
   await app.listen(process.env.PORT || 3000);
